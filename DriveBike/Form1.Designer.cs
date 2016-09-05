8c251022f69e1e646bfc86c130f86eaee2d2bda1
al-metal <al-metal@bk.ru>
2016-09-05 13:37:10 +0400

    add in Form textBoxs & rtb

--- DriveBike/Form1.Designer.cs
+++ DriveBike/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rtbMiniText = new System.Windows.Forms.RichTextBox();
+            this.rtbFullText = new System.Windows.Forms.RichTextBox();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.tbKeywords = new System.Windows.Forms.TextBox();
+            this.lbMiniText = new System.Windows.Forms.Label();
+            this.lblFullText = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblKeywords = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbMiniText
+            // 
+            this.rtbMiniText.Location = new System.Drawing.Point(12, 27);
+            this.rtbMiniText.Name = "rtbMiniText";
+            this.rtbMiniText.Size = new System.Drawing.Size(668, 182);
+            this.rtbMiniText.TabIndex = 0;
+            this.rtbMiniText.Text = "";
+            // 
+            // rtbFullText
+            // 
+            this.rtbFullText.Location = new System.Drawing.Point(12, 228);
+            this.rtbFullText.Name = "rtbFullText";
+            this.rtbFullText.Size = new System.Drawing.Size(668, 182);
+            this.rtbFullText.TabIndex = 1;
+            this.rtbFullText.Text = "";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(12, 426);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(668, 20);
+            this.tbTitle.TabIndex = 2;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(12, 465);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(668, 20);
+            this.tbDescription.TabIndex = 3;
+            // 
+            // tbKeywords
+            // 
+            this.tbKeywords.Location = new System.Drawing.Point(12, 501);
+            this.tbKeywords.Name = "tbKeywords";
+            this.tbKeywords.Size = new System.Drawing.Size(668, 20);
+            this.tbKeywords.TabIndex = 4;
+            // 
+            // lbMiniText
+            // 
+            this.lbMiniText.AutoSize = true;
+            this.lbMiniText.Location = new System.Drawing.Point(12, 9);
+            this.lbMiniText.Name = "lbMiniText";
+            this.lbMiniText.Size = new System.Drawing.Size(138, 13);
+            this.lbMiniText.TabIndex = 5;
+            this.lbMiniText.Text = "Краткое описание товара";
+            // 
+            // lblFullText
+            // 
+            this.lblFullText.AutoSize = true;
+            this.lblFullText.Location = new System.Drawing.Point(12, 212);
+            this.lblFullText.Name = "lblFullText";
+            this.lblFullText.Size = new System.Drawing.Size(134, 13);
+            this.lblFullText.TabIndex = 6;
+            this.lblFullText.Text = "Полное описание товара";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 410);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(113, 13);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Заголовок страницы";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 449);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(57, 13);
+            this.lblDescription.TabIndex = 8;
+            this.lblDescription.Text = "Описание";
+            // 
+            // lblKeywords
+            // 
+            this.lblKeywords.AutoSize = true;
+            this.lblKeywords.Location = new System.Drawing.Point(12, 488);
+            this.lblKeywords.Name = "lblKeywords";
+            this.lblKeywords.Size = new System.Drawing.Size(92, 13);
+            this.lblKeywords.TabIndex = 9;
+            this.lblKeywords.Text = "Ключевые слова";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(876, 524);
+            this.Controls.Add(this.lblKeywords);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblFullText);
+            this.Controls.Add(this.lbMiniText);
+            this.Controls.Add(this.tbKeywords);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.rtbFullText);
+            this.Controls.Add(this.rtbMiniText);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DriveBike";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMiniText;
+        private System.Windows.Forms.RichTextBox rtbFullText;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.TextBox tbKeywords;
+        private System.Windows.Forms.Label lbMiniText;
+        private System.Windows.Forms.Label lblFullText;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblKeywords;
     }
 }
 
