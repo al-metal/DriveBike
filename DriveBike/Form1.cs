6e523daa13b28e9f33bcb74f87a3f439f61e15fd
al-metal <al-metal@bk.ru>
2016-09-06 12:24:10 +0400

    download "naSite"

--- DriveBike/Form1.cs
+++ DriveBike/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -17,8 +18,9 @@ namespace DriveBike
 {
     public partial class Form1 : Form
     {
-        WebRequest webRequest = new WebRequest();
+        web.WebRequest webRequest = new web.WebRequest();
         CHPU chpu = new CHPU();
+        int addCount = 0;
         string boldOpen = "<span style=\"\"font-weight: bold; font-weight: bold; \"\">";
         string boldClose = "</span>";
         double discounts = 0.02;
@@ -272,6 +274,60 @@ namespace DriveBike
                     //Если разное кол-во ссылок на товар и наличия товара
                 }
             }
+            System.Threading.Thread.Sleep(20000);
+            string trueOtv = null;
+            string[] naSite1 = File.ReadAllLines("naSite.csv", Encoding.GetEncoding(1251));
+            if (naSite1.Length > 1)
+            {
+                do
+                {
+                    string otvimg = DownloadNaSite();
+                    string check = "{\"success\":true,\"imports\":{\"state\":1,\"errorCode\":0,\"errorLine\":0}}";
+                    do
+                    {
+                        System.Threading.Thread.Sleep(2000);
+                        otvimg = ChekedLoading();
+                    }
+                    while (otvimg == check);
+
+                    trueOtv = new Regex("(?<=\":{\"state\":).*?(?=,\")").Match(otvimg).ToString();
+                    string error = new Regex("(?<=errorCode\":).*?(?=,\")").Match(otvimg).ToString();
+                    if (error == "13")
+                    {
+                        string errstr = new Regex("(?<=errorLine\":).*?(?=,\")").Match(otvimg).ToString();
+                        string[] naSite = File.ReadAllLines("naSite.csv", Encoding.GetEncoding(1251));
+                        int u = Convert.ToInt32(errstr) - 1;
+                        string[] strslug3 = naSite[u].ToString().Split(';');
+                        string strslug = strslug3[strslug3.Length - 5];
+                        int slug = strslug.Length;
+                        int countAdd = ReturnCountAdd();
+                        int countDel = countAdd.ToString().Length;
+                        string strslug2 = strslug.Remove(slug - countDel);
+                        strslug2 += countAdd;
+                        naSite[u] = naSite[u].Replace(strslug, strslug2);
+                        File.WriteAllLines("naSite.csv", naSite, Encoding.GetEncoding(1251));
+                    }
+                    if (error == "37")
+                    {
+                        string errstr = new Regex("(?<=errorLine\":).*?(?=,\")").Match(otvimg).ToString();
+                        string[] naSite = File.ReadAllLines("naSite.csv", Encoding.GetEncoding(1251));
+                        int u = Convert.ToInt32(errstr) - 1;
+                        string[] strslug3 = naSite[u].ToString().Split(';');
+                        string strslug = strslug3[strslug3.Length - 5];
+                        int slug = strslug.Length;
+                        int countAdd = ReturnCountAdd();
+                        int countDel = countAdd.ToString().Length;
+                        string strslug2 = strslug.Remove(slug - countDel);
+                        strslug2 += countAdd;
+                        naSite[u] = naSite[u].Replace(strslug, strslug2);
+                        File.WriteAllLines("naSite.csv", naSite, Encoding.GetEncoding(1251));
+                    }
+                    if (error == "10")
+                    {
+                    }
+                }
+                while (trueOtv != "2");
+            }
         }
 
         private List<string> newList()
@@ -365,6 +421,58 @@ namespace DriveBike
                 text = text.Remove(text.LastIndexOf(" "));
             }
             return text;
+        }
+
+        private int ReturnCountAdd()
+        {
+            if (addCount == 99)
+                addCount = 0;
+            addCount++;
+            return addCount;
+        }
+
+        public string DownloadNaSite()
+        {
+            CookieContainer cookie = webRequest.webCookieBike18();
+            string epoch = (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds.ToString().Replace(",", "");
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("http://bike18.nethouse.ru/api/export-import/import-from-csv?fileapi" + epoch);
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
+            req.Method = "POST";
+            req.ContentType = "multipart/form-data; boundary=---------------------------12709277337355";
+            req.CookieContainer = cookie;
+            req.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            byte[] csv = File.ReadAllBytes("naSite.csv");
+            byte[] end = Encoding.ASCII.GetBytes("\r\n-----------------------------12709277337355\r\nContent-Disposition: form-data; name=\"_catalog_file\"\r\n\r\nnaSite.csv\r\n-----------------------------12709277337355--\r\n");
+            byte[] ms1 = Encoding.ASCII.GetBytes("-----------------------------12709277337355\r\nContent-Disposition: form-data; name=\"catalog_file\"; filename=\"naSite.csv\"\r\nContent-Type: text/csv\r\n\r\n");
+            req.ContentLength = ms1.Length + csv.Length + end.Length;
+            Stream stre1 = req.GetRequestStream();
+            stre1.Write(ms1, 0, ms1.Length);
+            stre1.Write(csv, 0, csv.Length);
+            stre1.Write(end, 0, end.Length);
+            stre1.Close();
+            HttpWebResponse resimg = (HttpWebResponse)req.GetResponse();
+            StreamReader ressrImg = new StreamReader(resimg.GetResponseStream());
+            string otvimg = ressrImg.ReadToEnd();
+            return otvimg;
+        }
+
+        public string ChekedLoading()
+        {
+            CookieContainer cookie = webRequest.webCookieBike18();
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("http://bike18.nethouse.ru/api/export-import/check-import");
+            req.Accept = "application/json, text/plain, */*";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
+            req.Method = "POST";
+            req.ContentLength = 0;
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.CookieContainer = cookie;
+            Stream stre1 = req.GetRequestStream();
+            stre1.Close();
+            HttpWebResponse resimg = (HttpWebResponse)req.GetResponse();
+            StreamReader ressrImg = new StreamReader(resimg.GetResponseStream());
+            string otvimg = ressrImg.ReadToEnd();
+            return otvimg;
         }
     }
 }
